d772ddbd542a8aeebc8502684718dae726864207
Nitin Dubey <nitindubey@Nitins-MacBook-Air.local>
2025-12-20 22:07:45 +0530

    requirements updated

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Dubey%40421999@localhost:5432/postgres"
+# DATABASE_URL = "postgresql://postgres:Dubey%40421999@localhost:5432/postgres"
+# DATABASE_URL = "postgresql://postgres_dhoc_user:owS0lTuGWkEAb9cfya2ijm7DuOasH0TY@dpg-d53ck875r7bs73do87u0-a.oregon-postgres.render.com/postgres_dhoc"
+DATABASE_URL = "postgresql://postgres_dhoc_user:owS0lTuGWkEAb9cfya2ijm7DuOasH0TY@dpg-d53ck875r7bs73do87u0-a/postgres_dhoc"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
